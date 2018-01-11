1a89f7406cf5e0f1f81ddb8a2d5e7e749f1591de
Jes Constantine <jesconstantine@gmail.com>
2018-01-11 16:57:28 -0500

    Add context docs for calculator widget js

--- js/form-calculator.js
+++ js/form-calculator.js
@@ -4,6 +4,10 @@
 (function($){
 // Handle the form submission
 $( "#ret__calculator__form" ).submit(function( event ) {
+  event.preventDefault();
+  // Hide the error messages if there are any (this is a bug in Mayflower)
+
+  // Populate the allowance values (indicated by a custom .js-ret-* class in the markup)
   $(".js-ret-annual").text("32,000");
   $(".js-ret-monthly").text("2,666");
 });
