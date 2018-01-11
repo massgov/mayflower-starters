bcbb24b061ee758e0afd428ba476b90d9e4e0502
Jes Constantine <jesconstantine@gmail.com>
2018-01-11 16:45:42 -0500

    Add allowance values on form submit (test)

--- js/form-calculator.js
+++ js/form-calculator.js
@@ -4,7 +4,7 @@
 (function($){
 // Handle the form submission
 $( "#ret__calculator__form" ).submit(function( event ) {
-  console.log( "Handler for .submit() called." );
-  event.preventDefault();
+  $(".js-ret-annual").text("32,000");
+  $(".js-ret-monthly").text("2,666");
 });
 })(jQuery);
