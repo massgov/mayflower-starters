58288daf1e2cb526d41e1b50467551f227145fb6
Jes Constantine <jesconstantine@gmail.com>
2018-01-12 08:43:00 -0500

    Scaffold out dynamic data iframe widget example

---
+++ iframe/dynamic-data/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Custom form calculator widget javascript
+ */
+(function($){
+// Handle the form submission
+$( "#ret__calculator__form" ).submit(function( event ) {
+  event.preventDefault();
+  // Hide the error messages if there are any (@TODO this should be handled by Mayflower js)
+  $(".js-error-list").hide();
+
+  // Populate the allowance values (indicated by a custom .js-ret-* class in the markup)
+  $(".js-ret-annual").text("32,000");
+  $(".js-ret-monthly").text("2,666");
+
+});
+})(jQuery);
