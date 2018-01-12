3a6b78943f079d0f48e7e90feb8d8d27348f1581
Jes Constantine <jesconstantine@gmail.com>
2018-01-12 07:24:03 -0500

    Hide the error list on form submit

--- js/form-calculator.js
+++ js/form-calculator.js
@@ -5,10 +5,12 @@
 // Handle the form submission
 $( "#ret__calculator__form" ).submit(function( event ) {
   event.preventDefault();
-  // Hide the error messages if there are any (this is a bug in Mayflower)
+  // Hide the error messages if there are any (@TODO this should be handled by Mayflower js)
+  $(".js-error-list").hide();
 
   // Populate the allowance values (indicated by a custom .js-ret-* class in the markup)
   $(".js-ret-annual").text("32,000");
   $(".js-ret-monthly").text("2,666");
+
 });
 })(jQuery);
