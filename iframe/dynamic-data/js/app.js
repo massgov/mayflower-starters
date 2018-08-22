53293b9fe781a0c2b693e3214dace6e91d606e0e
Minghua Sun <clairesunstudio@gmail.com>
2018-08-22 10:22:43 -0400

    add search js

--- iframe/dynamic-data/js/app.js
+++ iframe/dynamic-data/js/app.js
@@ -18,22 +18,3 @@
   });
 })(jQuery);
 // jQuery is available from vendor-generated.js
-
-// Forming search query
-(function($){
-  $(".js-header-search-form").each(function(){
-    // Cache jQuery objects
-    var $el = $(this),
-        $input = $el.find("input"),
-        $button = $el.find(".ma__button-search");
-
-        // Handle button click event
-      $button.on('click',function(e){
-        e.preventDefault();
-        var $query = encodeURIComponent($input[0].value),
-            $path = 'https://search.mass.gov/?q=',
-            $location = $path.concat($query);
-        window.location.href = $location;
-      });
-  });
-})(jQuery);
